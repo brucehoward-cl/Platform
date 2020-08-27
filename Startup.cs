3bdd9b08d8cb5f69d59ac91654ebb2ce030414af
Bruce Howard <brucehoward@twc.com>
2020-08-27 15:57:31 -0400

    Beginning static file services. Through Figure 15-13.

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.FileProviders;
 
 namespace Platform
 {
@@ -32,6 +33,13 @@ namespace Platform
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseStaticFiles();
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider($"{env.ContentRootPath}/staticfiles"), RequestPath = "/files"
+            });
+
             app.UseRouting();
             app.UseMiddleware<LocationMiddleware>();
             app.UseEndpoints(endpoints => {
