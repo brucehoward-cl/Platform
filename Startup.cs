dff3b49de85e202fb4e82cd911b23e75702b8820
Bruce Howard <brucehoward@twc.com>
2020-08-29 16:43:13 -0400

    Chapter 16 initial

--- Startup.cs
+++ Startup.cs
@@ -1,91 +1,119 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.FileProviders;
 
 namespace Platform
 {
     public class Startup
     {
-        public Startup(IConfiguration configService)
-        {
-            Configuration = configService; //The configuration service is instantiated BEFORE Startup is instantiated
-        }
-        private IConfiguration Configuration { get; set; }
-
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<MessageOptions>(Configuration.GetSection("Location")); //replaces the default values
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            app.UseStaticFiles();
-
-            app.UseStaticFiles(new StaticFileOptions
-            {
-                FileProvider = new PhysicalFileProvider($"{env.ContentRootPath}/staticfiles"), RequestPath = "/files"
-            });
-
+            app.UseDeveloperExceptionPage();
             app.UseRouting();
-            app.UseMiddleware<LocationMiddleware>();
             app.UseEndpoints(endpoints => {
-                endpoints.MapGet("/", async context => {
-                    logger.LogDebug("Response for / started");
-                    await context.Response.WriteAsync("Hello World!");
-                    logger.LogDebug("Response for / completed");
-                });
+                endpoints.MapFallback(async context =>
+                await context.Response.WriteAsync("Hello World!"));
             });
         }
-
-        #region Through Listing 15-19
-        ////        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,IConfiguration config)
-        //public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        //{
-        //    if (env.IsDevelopment())
-        //    {
-        //        app.UseDeveloperExceptionPage();
-        //    }
-        //    app.UseRouting();
-
-        //    app.UseMiddleware<LocationMiddleware>();
-
-        //    app.Use(async (context, next) =>
-        //    {
-        //        //string defaultDebug = config["Logging:LogLevel:Default"]; //This is used (without the need for a Configuration property) if only using it to configure middleware
-        //        string defaultDebug = Configuration["Logging:LogLevel:Default"];
-        //        await context.Response.WriteAsync($"The config setting is: {defaultDebug}");
-        //        string environ = Configuration["ASPNETCORE_ENVIRONMENT"];
-        //        await context.Response.WriteAsync($"\nThe env setting is: {environ}");
-        //        string wsID = Configuration["WebService:Id"];
-        //        string wsKey = Configuration["WebService:Key"];
-        //        await context.Response.WriteAsync($"\nThe secret ID is: {wsID}");
-        //        await context.Response.WriteAsync($"\nThe secret Key is: {wsKey}");
-        //    });
-        //    app.UseEndpoints(endpoints =>
-        //    {
-        //        endpoints.MapGet("/", async context =>
-        //        {
-        //            await context.Response.WriteAsync("Hello World!");
-        //        });
-        //    });
-        //} 
-        #endregion
     }
+
+
 }
+
+#region Chapter 15
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Builder;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.Hosting;
+//using Microsoft.Extensions.Options;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.Logging;
+//using Microsoft.Extensions.FileProviders; 
+//public class Startup
+//{
+//    public Startup(IConfiguration configService)
+//    {
+//        Configuration = configService; //The configuration service is instantiated BEFORE Startup is instantiated
+//    }
+//    private IConfiguration Configuration { get; set; }
+
+//    public void ConfigureServices(IServiceCollection services)
+//    {
+//        services.Configure<MessageOptions>(Configuration.GetSection("Location")); //replaces the default values
+//    }
+
+//    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+//    {
+//        if (env.IsDevelopment())
+//        {
+//            app.UseDeveloperExceptionPage();
+//        }
+//        app.UseStaticFiles();
+
+//        app.UseStaticFiles(new StaticFileOptions
+//        {
+//            FileProvider = new PhysicalFileProvider($"{env.ContentRootPath}/staticfiles"),
+//            RequestPath = "/files"
+//        });
+
+//        app.UseRouting();
+//        app.UseMiddleware<LocationMiddleware>();
+//        app.UseEndpoints(endpoints =>
+//        {
+//            endpoints.MapGet("/", async context =>
+//            {
+//                logger.LogDebug("Response for / started");
+//                await context.Response.WriteAsync("Hello World!");
+//                logger.LogDebug("Response for / completed");
+//            });
+//        });
+//    }
+
+//    #region Through Listing 15-19
+//    ////        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,IConfiguration config)
+//    //public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+//    //{
+//    //    if (env.IsDevelopment())
+//    //    {
+//    //        app.UseDeveloperExceptionPage();
+//    //    }
+//    //    app.UseRouting();
+
+//    //    app.UseMiddleware<LocationMiddleware>();
+
+//    //    app.Use(async (context, next) =>
+//    //    {
+//    //        //string defaultDebug = config["Logging:LogLevel:Default"]; //This is used (without the need for a Configuration property) if only using it to configure middleware
+//    //        string defaultDebug = Configuration["Logging:LogLevel:Default"];
+//    //        await context.Response.WriteAsync($"The config setting is: {defaultDebug}");
+//    //        string environ = Configuration["ASPNETCORE_ENVIRONMENT"];
+//    //        await context.Response.WriteAsync($"\nThe env setting is: {environ}");
+//    //        string wsID = Configuration["WebService:Id"];
+//    //        string wsKey = Configuration["WebService:Key"];
+//    //        await context.Response.WriteAsync($"\nThe secret ID is: {wsID}");
+//    //        await context.Response.WriteAsync($"\nThe secret Key is: {wsKey}");
+//    //    });
+//    //    app.UseEndpoints(endpoints =>
+//    //    {
+//    //        endpoints.MapGet("/", async context =>
+//    //        {
+//    //            await context.Response.WriteAsync("Hello World!");
+//    //        });
+//    //    });
+//    //} 
+//    #endregion
+//} 
+#endregion
+
 
 #region Chapter 14
 //using Microsoft.AspNetCore.Builder;
