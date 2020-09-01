cf897b18c6af17e9d8f8def6eb85f8695c056893
Bruce Howard <brucehoward@twc.com>
2020-09-01 11:04:35 -0400

    Through 16-9 Session cookies and into HTTPs redirection.

--- Startup.cs
+++ Startup.cs
@@ -10,46 +10,83 @@ namespace Platform
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(opts => { opts.CheckConsentNeeded = context => true; });
+            services.Configure<CookiePolicyOptions>(opts => { opts.CheckConsentNeeded = context => true; }); //for client cookies
+
+            services.AddDistributedMemoryCache();  //for session cookies
+            services.AddSession(options => {        //for session cookies
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.IsEssential = true;
+            });
         }
 
         public void Configure(IApplicationBuilder app)
         {
             app.UseDeveloperExceptionPage();
-            app.UseCookiePolicy();
-            app.UseMiddleware<ConsentMiddleware>();
+            app.UseHttpsRedirection(); //forces redirection to use https for http requests; can also use services.AddHttpsRedirection in ConfigureServices method for options
+            app.UseCookiePolicy();  //for client cookies
+            app.UseMiddleware<ConsentMiddleware>(); //for client cookies
+            app.UseSession();  //for session cookies
             app.UseRouting();
-            app.UseEndpoints(endpoints => {
-                endpoints.MapGet("/cookie", async context => {
-                    int counter1 = int.Parse(context.Request.Cookies["counter1"] ?? "0") + 1;
-                    context.Response.Cookies.Append("counter1", counter1.ToString(),
-                        new CookieOptions
-                        {
-                            MaxAge = TimeSpan.FromMinutes(30),
-                            IsEssential = true
-                        });
-                    int counter2 = int.Parse(context.Request.Cookies["counter2"] ?? "0") + 1;
-                    context.Response.Cookies.Append("counter2", counter1.ToString(),
-                        new CookieOptions
-                        {
-                            MaxAge = TimeSpan.FromMinutes(30)
-                        });
+
+            app.Use(async (context, next) => {
+                await context.Response.WriteAsync($"HTTPS Request: {context.Request.IsHttps} \n");
+                await next();
+            });
+
+            #region ClientCookies
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/cookie", async context =>
+            //    {
+            //        int counter1 = int.Parse(context.Request.Cookies["counter1"] ?? "0") + 1;
+            //        context.Response.Cookies.Append("counter1", counter1.ToString(),
+            //            new CookieOptions
+            //            {
+            //                MaxAge = TimeSpan.FromMinutes(30),
+            //                IsEssential = true
+            //            });
+            //        int counter2 = int.Parse(context.Request.Cookies["counter2"] ?? "0") + 1;
+            //        context.Response.Cookies.Append("counter2", counter1.ToString(),
+            //            new CookieOptions
+            //            {
+            //                MaxAge = TimeSpan.FromMinutes(30)
+            //            });
+            //        await context.Response.WriteAsync($"Counter1: {counter1}, Counter2: {counter2}");
+            //    });
+            //    endpoints.MapGet("clear", context =>
+            //    {
+            //        context.Response.Cookies.Delete("counter1");
+            //        context.Response.Cookies.Delete("counter2");
+            //        context.Response.Redirect("/");
+            //        return Task.CompletedTask;
+            //    });
+            //    endpoints.MapFallback(async context =>
+            //        await context.Response.WriteAsync("Hello World!"));
+            //}); 
+            #endregion
+
+            #region SessionCookies
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/cookie", async context =>
+                {
+                    int counter1 = (context.Session.GetInt32("counter1") ?? 0) + 1;
+                    int counter2 = (context.Session.GetInt32("counter2") ?? 0) + 1;
+                    context.Session.SetInt32("counter1", counter1);
+                    context.Session.SetInt32("counter2", counter2);
+                    await context.Session.CommitAsync();
                     await context.Response.WriteAsync($"Counter1: {counter1}, Counter2: {counter2}");
                 });
-                endpoints.MapGet("clear", context => {
-                    context.Response.Cookies.Delete("counter1");
-                    context.Response.Cookies.Delete("counter2");
-                    context.Response.Redirect("/");
-                    return Task.CompletedTask;
-                });
                 endpoints.MapFallback(async context =>
-                    await context.Response.WriteAsync("Hello World!"));
-            });
+                await context.Response.WriteAsync("Hello World!"));
+            }); 
+            #endregion
         }
     }
 
 
 }
+
 
 #region Chapter 15
 //using System;
